b99c39b47d45a5b55253ae371c868831a6a2bcbf
krystoham <krystof.ham@volny.cz>
2026-06-09 18:48:16 +0200

    Wuartz konfig push

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Kryštof Ham - Přednášky a zápisy",
+    pageTitle: "KH",
     pageTitleSuffix: "",
     enableSPA: false,
     enablePopovers: true,
